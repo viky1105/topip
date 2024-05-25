6f48961d7d3df8d36aa1282c14e1423ca47f2af4
Abor Victor <khinqviktor1111@gmail.com>
2024-05-24 22:07:24 -0700

    First commit of my website files

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("alertbutton").addEventListener("click", function () {
+    alert("you successlly created an order");
+});
+document.getElementById("sell").addEventListener("click", function () {
+    alert("order has been sold");
+});
