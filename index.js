c01bd3247c947ecf537968873a7a35ac8a10dcc2
viky1105 <khinqviktor1111@gmail.com>
2024-06-07 02:39:53 -0700

    update admin

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function handleLogin() {
     const username = document.getElementById('exampleInputEmail1').value;
     const password = document.getElementById('exampleInputPassword1').value;
+    const mockUsername = 'admin';
+    const mockPassword = 'password123';
 
     const registeredUsername = localStorage.getItem('registeredUsername');
     const registeredPassword = localStorage.getItem('registeredPassword');
@@ -9,6 +11,13 @@ function handleLogin() {
         alert('Login successful!');
         localStorage.setItem('username', username);
         window.location.href = 'dashboard.html';
+    } else if (username === mockUsername && password === mockPassword) {
+         // Store a flag in localStorage to indicate that the user is logged in
+         localStorage.setItem('isAdminLoggedIn', 'true');
+         // Redirect to admin page
+         window.location.href = 'admin.html';
+       
+
     } else {
 
         alert('Invalid username or password');
